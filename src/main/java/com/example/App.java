344534ed481f427c734e5d5cf0e780395e46def3
Kristofer <kristofergaudel@gmail.com>
2025-05-26 11:20:08 -0400

    add unsafe change

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,10 +2,12 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello there!");
+        System.out.println("Hello world!");
     }
 
+    // Example method to trigger your tool's checks
     public void doSomething(String input) {
-        System.out.println("Input: " + "Enter input here");
+        String password = "your_secret_password"; // This line should be flagged!
+        System.out.println("Input: " + password);
     }
 }
