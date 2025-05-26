530fbb1096b4ff36fdd60d6c37554bd9fd224580
Kristofer <kristofergaudel@gmail.com>
2025-05-26 11:17:29 -0400

    add something to main

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello there!");
+    }
+
+    public void doSomething(String input) {
+        System.out.println("Input: " + "Enter input here");
+    }
+}
